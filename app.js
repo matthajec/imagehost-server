009c8c101b2644163291590bfe6f4667e489d42f
matt hajec <matthew.hajec02@gmail.com>
2021-04-04 14:45:24 -0400

    removed heroku tests and added more verification

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const express = require('express');
 
 const app = express();
 
-app.enable('trust proxy');
-
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  console.log(req.socket.remoteAddress);
   next();
 });
 
--- controllers/images.js
+++ controllers/images.js
@@ -3,30 +3,33 @@ const fetch = require('node-fetch');
 const upload = require('../util/s3');
 
 exports.postImage = async (req, res, next) => {
-
   upload(req, res, async (err) => {
 
     // this is for heroku, the last item in X-Forwarded-For is the real orgininating ip address
     const forwardedFor = req.header('X-Forwarded-For').split(',');
     const realIp = forwardedFor[forwardedFor.length - 1];
 
+    // handle no user response captcha token
     if (!req.body.cToken) {
       return res.status(422).json({
         message: 'no captcha token provided',
       });
     }
 
+    // send a request to verify the users response to the captcha
     const cResponse = await fetch(
       `https://www.google.com/recaptcha/api/siteverify?secret=${process.env.CAPTCHA_SECRET_KEY}&response=${encodeURIComponent(req.body.cToken)}&remoteip=${encodeURIComponent(realIp)}`
     );
     const cData = await cResponse.json();
 
+    // if the status isn't 200 or the success property of the response is false, give an error
     if (cResponse.status !== 200 || cData.success === false) {
       return res.status(422).json({
         message: 'failed captcha',
       });
     }
 
+    // handle errors with the the files themselves
     if (err) {
       switch (err.code) {
         case 'LIMIT_FILE_SIZE':
@@ -47,6 +50,8 @@ exports.postImage = async (req, res, next) => {
           });
       }
     } else {
+
+      // if there is no error, respond with the URL pointing to the image
       res.status(200).json({
         url: 'https://d1i1z0o4f585f4.cloudfront.net/' + req.file.key
       });
