28b13f4649574b7411b8c560f19503f7101c27d3
matt hajec <matthew.hajec02@gmail.com>
2021-04-04 13:48:51 -0400

    heroku proxy test

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   res.setHeader('Access-Control-Allow-Headers', '*');
-  console.log(req.socket.ip);
+  console.log(req.socket.remoteAddress);
   next();
 });
 
