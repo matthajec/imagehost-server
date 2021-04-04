9880004a6a4ee05f19a12c0c6c4fc25e24b90cff
matt hajec <matthew.hajec02@gmail.com>
2021-04-03 20:17:28 -0400

    minor refactoring

--- app.js
+++ app.js
@@ -5,10 +5,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'OPTIONS, POST'
-  );
+  res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, POST');
   next();
 });
 
--- controllers/images.js
+++ controllers/images.js
@@ -7,7 +7,6 @@ exports.postImage = async (req, res, next) => {
     console.log(req.file);
 
     if (err) {
-      console.log(err);
       switch (err.code) {
         case 'LIMIT_FILE_SIZE':
           return res.status(422).json({
@@ -26,11 +25,9 @@ exports.postImage = async (req, res, next) => {
             message: 'An unknown error occured'
           });
       }
-
-
     } else {
       res.status(200).json({
-        url: 'd1i1z0o4f585f4.cloudfront.net/' + req.file.key
+        url: 'https://d1i1z0o4f585f4.cloudfront.net/' + req.file.key
       });
     }
   });
